614042581e39a0f97c80758c4a56a36abc104fe4
Mariana Camacho <marianacamacho700@gmail.com>
2022-03-31 01:15:45 -0500

    integrantes

---
+++ mariana.js
@@ -0,0 +1,15 @@
+const info= document.getElementById("f10")
+
+info.innerHTML += `
+<div class="col col-md-3 my-4">
+<div class="card">
+<img src="https://scontent.feoh4-3.fna.fbcdn.net/v/t39.30808-6/276294201_10227686823693607_2653888009030714099_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=730e14&_nc_eui2=AeGfIre7JO4E35_mRUFVd-qb6b_xD46iY47pv_EPjqJjjstkBFsWWBCWeTeYGRwOpoY&_nc_ohc=f9L0Hvc7lHsAX-H0tI6&_nc_ht=scontent.feoh4-3.fna&oh=00_AT_PP41aBY9_3v70om4H8GRKj4psuiWN_HtdtxflQTt6gw&oe=62462B6D" >
+<h1> Mariana Camacho</h1>
+<h3> Edad: 33</h3>
+<h3> Nacionalidad: Venezolana</h3>
+<h3> Vivi:Cabimas, Edo Zulia</h3>
+<h3> Vivo en: Medellin</h3>
+<p> Soy ing de sistemas, no ejerci nunca mi profesion,lo unico que se de programacion fue lo que vi en la universidad, sin embargo siempre me gusto  y decidi retomar lo que me gusta y por eso estoy aca </p>
+</div>
+</div>
+`
